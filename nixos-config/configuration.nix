65681230b5380a40b3467c5bdb46472fda44db30
Nikhil Kumar <122196625+nkv994@users.noreply.github.com>
2026-07-01 09:15:18 +0530

    Add killall to system packages

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -103,6 +103,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    killall
     waybar          # Status bar
     wofi            # App launcher (or rofi-wayland)
     dunst           # Notifications
